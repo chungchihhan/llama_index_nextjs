b98213365296800806ec7b2db81beaea9f1bb7db
chungchihhan <harryup2000@gmail,com>
2024-01-07 00:37:43 +0800

    fix type error

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,5 @@
 import { Message, StreamingTextResponse } from "ai";
-import { MessageContent, OpenAI } from "llamaindex";
+import { ChatMessage, MessageContent, MessageType, OpenAI } from "llamaindex";
 import { NextRequest, NextResponse } from "next/server";
 import { createChatEngine } from "./engine";
 import { LlamaIndexStream } from "./llamaindex-stream";
@@ -9,7 +9,7 @@ export const dynamic = "force-dynamic";
 
 const getLastMessageContent = (
   textMessage: string,
-  imageUrl: string | undefined,
+  imageUrl: string | undefined
 ): MessageContent => {
   if (!imageUrl) return textMessage;
   return [
@@ -37,12 +37,20 @@ export async function POST(request: NextRequest) {
           error:
             "messages are required in the request body and the last message must be from the user",
         },
-        { status: 400 },
+        { status: 400 }
       );
     }
 
     const llm = new OpenAI({
-      model: process.env.MODEL || "gpt-3.5-turbo",
+      model:
+        (process.env.MODEL as
+          | "gpt-3.5-turbo"
+          | "gpt-3.5-turbo-16k"
+          | "gpt-3.5-turbo-1106"
+          | "gpt-4"
+          | "gpt-4-32k"
+          | "gpt-4-1106-preview"
+          | "gpt-4-vision-preview") || "gpt-3.5-turbo",
       maxTokens: 512,
     });
 
@@ -50,13 +58,21 @@ export async function POST(request: NextRequest) {
 
     const lastMessageContent = getLastMessageContent(
       lastMessage.content,
-      data?.imageUrl,
+      data?.imageUrl
     );
 
+    const convertToChatMessage = function (message: Message): ChatMessage {
+      return {
+        content: message.content,
+        role: message.role as MessageType, // 这里假设 Message$1 的 role 字段与 MessageType 兼容
+      };
+    };
+
+    const chatMessages: ChatMessage[] = messages.map(convertToChatMessage);
     const response = await chatEngine.chat(
       lastMessageContent as MessageContent,
-      messages,
-      true,
+      chatMessages,
+      true
     );
 
     // Transform the response into a readable stream
@@ -72,7 +88,7 @@ export async function POST(request: NextRequest) {
       },
       {
         status: 500,
-      },
+      }
     );
   }
 }
