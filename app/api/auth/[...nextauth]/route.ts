53953ebfd2bde456c99827cb53f98032749dca08
chungchihhan <harryup2000@gmail,com>
2024-01-08 00:45:07 +0800

    next/server error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,15 @@
 // api/auth/[...nextauth]/route.ts
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 import NextAuth from "@/app/lib/auth";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
   if (req.method === "GET") {
     // Handle GET request
     return await NextAuth(req, res);
   }
 }
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(req: NextRequest, res: NextResponse) {
   if (req.method === "POST") {
     // Handle POST request
     return await NextAuth(req, res);
