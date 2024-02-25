bfe0e085fab755a70436c270fd2d6b289163087b
chungchihhan <harryup2000@gmail,com>
2024-02-25 13:29:06 +0800

    UI : make the drag box clickable and edit some ui

--- src/app/ScrapePage/SaveTXTPage/page.tsx
+++ src/app/ScrapePage/SaveTXTPage/page.tsx
@@ -13,15 +13,15 @@ export default function Home() {
   }
 
   const onDrop = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault(); // 防止瀏覽器的默認處理方式（比如打開文件）
-    setIsDragging(false); // 更新拖拽狀態
+    e.preventDefault();
+    setIsDragging(false);
 
     if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
-      setFile(e.dataTransfer.files[0]); // 取得拖拽的第一個文件並更新狀態
+      setFile(e.dataTransfer.files[0]);
       setFilename(e.dataTransfer.files[0].name);
-      console.log("File dragged:", e.dataTransfer.files[0].name); // 可以在控制台輸出文件名來確認
+      console.log("File dragged:", e.dataTransfer.files[0].name);
     } else {
-      console.log("No files were dragged into the area."); // 如果沒有文件，輸出提示信息
+      console.log("No files were dragged into the area.");
     }
   };
 
@@ -33,6 +33,17 @@ export default function Home() {
   const onDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     setIsDragging(false);
+  };
+
+  const onFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setFile(e.target.files[0]);
+      setFilename(e.target.files[0].name);
+    }
+  };
+
+  const handleDivClick = () => {
+    document.getElementById("fileInput")?.click();
   };
 
   const OnSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -54,21 +65,23 @@ export default function Home() {
   };
 
   return (
-    <main>
-      <form onSubmit={OnSubmit}>
+    <main className="flex min-h-screen flex-col background-gradient justify-center">
+      <form
+        onSubmit={OnSubmit}
+        className="flex flex-col justify-center items-center gap-5"
+      >
         <div
+          onClick={handleDivClick} // 添加點擊事件處理器
           onDrop={onDrop}
           onDragOver={onDragOver}
           onDragLeave={onDragLeave}
           style={{
             border: isDragging ? "3px dashed #000" : "3px dashed #aaa",
-            padding: "20px",
             textAlign: "center",
-            marginBottom: "20px",
           }}
-          className="hover:bg-slate-100"
+          className="hover:backdrop-blur-md hover:bg-white/30 self-center flex h-80 px-12 rounded-xl flex-col justify-center cursor-pointer"
         >
-          拖拽文件到這裡或點擊下方選擇文件
+          拖拽文件到這裡或點擊上傳文件
           {filename && <p className="text-red-500">{filename}</p>}
         </div>
         <input
@@ -76,16 +89,13 @@ export default function Home() {
           name="file"
           style={{ display: "none" }}
           id="fileInput"
-          onChange={(e) => setFile(e.target.files?.[0])}
+          onChange={onFileInputChange}
         />
-        <label
-          htmlFor="fileInput"
-          style={{ cursor: "pointer" }}
-          className="hover:bg-slate-300"
-        >
-          選擇文件
-        </label>
-        <input type="submit" value="Upload" className="hover:bg-slate-300" />
+        <input
+          type="submit"
+          value="Upload"
+          className="hover:bg-slate-300 flex px-16 py-1 bg-slate-100 rounded-xl cursor-pointer"
+        />
       </form>
     </main>
   );
